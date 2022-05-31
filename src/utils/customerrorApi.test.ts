7a275fe1bf394061d961f29e1d35dee8f4da73c9
Abraham Saavedra <abrasm92@gmail.com>
2022-05-31 20:00:50 +0200

    tests custom error

--- src/utils/customerrorApi.test.ts
+++ src/utils/customerrorApi.test.ts
@@ -17,4 +17,52 @@ describe("Given a customErrorApi function", () => {
       expect(message).toEqual(expectedMessage);
     });
   });
+
+  describe("When it's invoked with response status 409 and message 'Invalid email'", () => {
+    test("Then it should return 'Invalid username'", () => {
+      const error: customError = {
+        response: {
+          status: 409,
+          data: { message: "Invalid email" },
+        },
+      };
+      const expectedMessage = "Invalid email";
+
+      const message = customErrorApi(error);
+
+      expect(message).toEqual(expectedMessage);
+    });
+  });
+
+  describe("When it's invoked with response status 500 and message 'Server error'", () => {
+    test("Then it should return 'Invalid username'", () => {
+      const error: customError = {
+        response: {
+          status: 500,
+          data: { message: "Server error" },
+        },
+      };
+      const expectedMessage = "Server error";
+
+      const message = customErrorApi(error);
+
+      expect(message).toEqual(expectedMessage);
+    });
+  });
+
+  describe("When it's invoked with response status not covered in function ex:300 and message ''", () => {
+    test("Then it should return 'Invalid username'", () => {
+      const error: customError = {
+        response: {
+          status: 300,
+          data: { message: "" },
+        },
+      };
+      const expectedMessage = "";
+
+      const message = customErrorApi(error);
+
+      expect(message).toEqual(expectedMessage);
+    });
+  });
 });
